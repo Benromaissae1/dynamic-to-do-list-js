1ec709e4195364dcb5cb1ac03c9295380dc10c84
benromaissae1 <benromaissae1@gmail.com>
2025-08-03 17:36:58 +0200

    tasks

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('add-task');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask(){
+        const taskText = taskInput.value.trim();
+
+        if (taskText === "") {
+            alert("enter a task");
+            return;
+        }
+        const listItem = document.createElement('li');
+        listItem.textContent = taskText;
+
+        const deleteButton = document.createElement(Button);
+        deleteButton.textContent = 'Remove';
+        deleteButton.className = 'remove-btn'
+
+        deleteButton.onclick = function () {
+            taskList.removeChild(listItem);
+        };
+        listItem.appendChild(deleteButton);
+
+        taskList.appendChild(listItem);
+
+        taskInput.value = '';
+
+    }
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+          addTask();
+        }
+      });
+  });

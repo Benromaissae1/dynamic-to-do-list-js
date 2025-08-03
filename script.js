9406d34779824f7eaa948254c28fc2bd6c8d109d
benromaissae1 <benromaissae1@gmail.com>
2025-08-03 17:38:47 +0200

    tasks

--- script.js
+++ script.js
@@ -3,34 +3,37 @@ document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
-    function addTask(){
+    function addTask() {
         const taskText = taskInput.value.trim();
 
         if (taskText === "") {
             alert("enter a task");
             return;
         }
+
         const listItem = document.createElement('li');
         listItem.textContent = taskText;
 
-        const deleteButton = document.createElement(Button);
+        // إنشاء زر الحذف (لاحظ استخدام 'button' كسلسلة نصية)
+        const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Remove';
-        deleteButton.className = 'remove-btn'
+        deleteButton.className = 'remove-btn';
 
         deleteButton.onclick = function () {
             taskList.removeChild(listItem);
         };
-        listItem.appendChild(deleteButton);
 
+        listItem.appendChild(deleteButton);
         taskList.appendChild(listItem);
 
         taskInput.value = '';
-
     }
+
     addButton.addEventListener('click', addTask);
+
     taskInput.addEventListener('keypress', function (event) {
         if (event.key === 'Enter') {
-          addTask();
+            addTask();
         }
-      });
-  });
+    });
+});
